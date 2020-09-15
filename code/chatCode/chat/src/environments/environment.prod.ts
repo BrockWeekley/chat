3c7b88a075428e62bdabcf1b72657a5dfa885a98
BrockWeekley <48456042+BrockWeekley@users.noreply.github.com>
2020-09-15 09:23:18 -0500

    Update environment.prod.ts

--- code/chatCode/chat/src/environments/environment.prod.ts
+++ code/chatCode/chat/src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: 'AIzaSyA43sfyNjYdqMi3tg_zghN2Z5DurS_MGFk',
-    authDomain: 'chat-afa8c.firebaseapp.com',
-    databaseURL: 'https://chat-afa8c.firebaseio.com',
-    projectId: 'chat-afa8c',
-    storageBucket: '',
-    messagingSenderId: '804735711813',
-    appId: '1:804735711813:web:7c1015c3218c0c9f53412c'
-  }
+// Firebase Config here
 };
